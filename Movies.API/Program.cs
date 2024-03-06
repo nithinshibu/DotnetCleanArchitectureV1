662599f11b659737b514bdca4ebbff9385310296
Nithin Mathew <nithin.mathew@sysfore.com>
2024-03-06 17:23:52 +0530

    Configured Program.cs

--- Movies.API/Program.cs
+++ Movies.API/Program.cs
@@ -1,11 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Movies.Application.Handlers;
+using Movies.Application.Mappers;
+using Movies.Core.Repositories;
+using Movies.Core.Repositories.Base;
+using Movies.Infrastructure.Data;
+using Movies.Infrastructure.Repositories;
+using Movies.Infrastructure.Repositories.Base;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<MovieContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseSqlServer(connectionString);
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(typeof(MovieMappingProfile));
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+builder.Services.AddScoped(typeof(IRepository<>),typeof(Repository<>));
+
+
+builder.Services.AddTransient<IMovieRepository, MovieRepository>();
 
 var app = builder.Build();
 
@@ -22,4 +48,27 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+var host = app.Services.GetRequiredService<IHost>();
+await CreateAndSeedDb(host);
+
 app.Run();
+
+async Task CreateAndSeedDb(IHost host)
+{
+    using (var scope = host.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+        try
+        {
+            var moviesContext = services.GetRequiredService<MovieContext>();
+            await MovieContextSeed.SeedAsync(moviesContext, loggerFactory);
+        }
+        catch (Exception ex)
+        {
+            var logger = loggerFactory.CreateLogger<Program>();
+            logger.LogError($"Exception occured in API: {ex.Message}");
+        }
+    }
+
+}
