eff32a1ba74ea37fcd918cc619721c1e3696e9bc
Nithin Mathew <nithin.mathew@sysfore.com>
2024-03-06 11:42:39 +0530

    Data setup in infrastructure completed

---
+++ Movies.Infrastructure/Data/MovieContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Infrastructure.Data
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options)
+        {
+        }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
